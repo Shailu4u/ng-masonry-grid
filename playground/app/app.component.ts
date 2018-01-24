9bd0531e4b1207bfd670e5ca7c80ca8b709e4220
Shailendra Pallikonda <Shailendra_Pallikonda@epam.com>
2018-01-24 17:17:10 +0530

    Updated playground with dynamic grid items.

--- playground/app/app.component.ts
+++ playground/app/app.component.ts
@@ -4,24 +4,68 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-    <ng-masonry-grid
+    <ul class="list">
+      <li class="list-item" *ngFor="let item of buttons">
+        <button class="btn" [ngClass]="{ 'active': item.active }" (click)="onSelect(item)">Effect {{item.index}}</button>
+      </li>
+    </ul>
+  </div>
+  <div class="container">
+    <ng-masonry-grid *ngIf="showMasonry"
                   [masonryOptions]="{ transitionDuration: '0.4s', gutter: 5 }"
                   [useAnimation]="true"
                   [useImagesLoaded]="true"
-                  [scrollAnimationOptions]="{ animationEffect: 'effect-6', minDuration : 0.4, maxDuration : 0.7 }">
+                  [scrollAnimationOptions]="animOptions">
       <ng-masonry-grid-item *ngFor="let item of masonryItems; let i = index;">
-        <img src="../assets/images/{{ (i+1) >  15 ? masonryItems.length - i: i+1 }}.jpg" alt="No image" />
+        <img [src]="item" alt="No image" />
       </ng-masonry-grid-item>
     </ng-masonry-grid>
   </div>
 `,
-styleUrls: ['../../node_modules/ng-masonry-grid/ng-masonry-grid.css'] // point to ng masonry grid css
+styleUrls: ['../../node_modules/ng-masonry-grid/ng-masonry-grid.css', './app.component.css'] // point to ng masonry grid css
 })
 export class AppComponent {
 
-  masonryItems: Array<number> = [];
+  buttons: Array<any> = [];
+
+  showMasonry = true;
+
+  animOptions = { animationEffect: 'effect-1' };
+
+  masonryItems: Array<string> = [];
 
   constructor() {
-    this.masonryItems.length = 30;
+    this.buttons = [
+      { index: 1, active: true },
+      { index: 2, active: false },
+      { index: 3, active: false },
+      { index: 4, active: false },
+      { index: 5, active: false },
+      { index: 6, active: false },
+      { index: 7, active: false },
+      { index: 8, active: false }
+    ];
+
+    const len = 40; // length of grid items
+
+    for (let i = 0; i <= len; i++) {
+      this.masonryItems.push(this.getSrc());
+    }
+  }
+
+  onSelect(item: any) {
+    this.showMasonry = false;
+    this.buttons.forEach( (i) => i.active = false );
+    item.active = true;
+    this.animOptions = { animationEffect: 'effect-' + item.index };
+    setTimeout(() => this.showMasonry = true, 100);
+  }
+
+  getRandomInt(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  getSrc() {
+    return '../assets/images/' + this.getRandomInt(1, 15) + '.jpg';
   }
 }
